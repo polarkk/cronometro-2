71bb024b60f0e8556927da167c0aee7681ed4957
polarkk <eduardo.dolatto@escola.pr.gov.br>
2026-05-21 15:21:06 +0000

    aula 4

--- main.js
+++ main.js
@@ -12,3 +12,10 @@ for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
+const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
+const tempoObjetivo1 = new Date("2024-10-05");
+
+contadores[0].textContent = tempoObjetivo1;
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
