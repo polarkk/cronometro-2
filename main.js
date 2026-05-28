ec60ce6245f169e5a351b9e89c9a075b6a0242c1
polarkk <eduardo.dolatto@escola.pr.gov.br>
2026-05-28 14:32:19 +0000

    Aula 05

--- main.js
+++ main.js
@@ -18,4 +18,21 @@ const tempoObjetivo1 = new Date("2024-10-05");
 
 contadores[0].textContent = tempoObjetivo1;
 let tempoAtual = new Date();
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual; 
+  let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+return (
+  dias +
+  " dias " +
+  horas +
+  " horas " +
+  minutos +
+  " minutos " +
+  segundos +
+  " segundos"
+);
+}
